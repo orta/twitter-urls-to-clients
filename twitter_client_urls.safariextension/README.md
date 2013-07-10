47845b28e40540693dcd326ae5e2d184960cb48c
Orta Therox <orta.therox@gmail.com>
2013-07-10 23:03:26 +0100

    initial 0.5 commit

---
+++ twitter_client_urls.safariextension/README.md
@@ -0,0 +1,4 @@
+twitter-urls-to-clients
+=======================
+
+A Safari extension to convert twitter.com urls to use mac clients instead
---
+++ twitter_client_urls.safariextension/twitter_urls_to_clients.js
@@ -0,0 +1,33 @@
+ // console.log(safari.extension.settings.client)
+
+function change_twitter_to_clients() {
+  var anchors = document.getElementsByTagName("a");
+  for (var i = 0; i < anchors.length; i++ ){
+
+    var anchor = anchors[i]
+    if(anchor.href){
+
+      var href = anchor.href
+      if(href.indexOf("twitter") != -1){      
+      
+        var components = href.split("/")
+        if(components.length == 4){
+            var username = components[3]
+            anchor.href = "tweetbot:///user_profile/" + username
+        }      
+      }
+    }
+  }
+}
+ 
+// We want to support dynamically added content so on every document change
+// do a quick check of the URLs. This will also most likely kick it to action initially.
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    change_twitter_to_clients()
+  });    
+});
+ 
+var config = { attributes: true, childList: true, characterData: true, subtree: true };
+observer.observe(document.body, config);
